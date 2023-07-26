9ceb3c5a95430e15ba6e78064be2f9141b39d48b
SS-hj <gkwjd111@naver.com>
2023-07-26 20:22:17 +0900

    Upload baekjoon

--- baekjoon/9465.py
+++ baekjoon/9465.py
@@ -1,16 +1,14 @@
 t = int(input())
 
-
 for _ in range(t):
     n = int(input())
-    arr = [list(map(int, input().split())) for _ in range(2)]
-    d0 = [[0]*n for _ in range(2)]
-    d1 = [[0]*n for _ in range(2)]
-    d0[0][0] = arr[0][0]
-    d1[1][0] = arr[1][0]
-    d1[0][1] = d1[1][0] + arr[0][1]
-    d0[1][1] = d0[0][0] + arr[1][1]
-    for i in range(2):
-        for j in range(2,n):
-            d0[i][j] = max(d0[1-i][j-1],d0[1-i][j-2]) + arr[i][j]
-    print(max(d0))
+    dp = [list(map(int, input().split())) for _ in range(2)]
+    if n==1:
+        print(max(dp[0][0], dp[1][0]))
+    else:
+        dp[0][1] += dp[1][0]
+        dp[1][1] += dp[0][0]
+        for i in range(2,n):
+            dp[0][i] += max(dp[1][i-1], dp[1][i-2])
+            dp[1][i] += max(dp[0][i-1], dp[0][i-2])
+        print(max(dp[0][n-1], dp[1][n-1]))
