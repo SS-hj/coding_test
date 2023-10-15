8430b8cae277f473710239bf6f9c4e5d5b4e25c1
SS-hj <gkwjd111@naver.com>
2023-10-15 21:52:35 +0900

    Update code

--- programmers/17677.py
+++ programmers/17677.py
@@ -1,31 +1,24 @@
-import re
-import math
 def solution(str1, str2):
-    p = re.compile('[a-z][a-z]')
-    str1 = str1.lower()
-    str2 = str2.lower()
-    check1 = []
-    check2 = []
+    check = dict()
+    total = 0
     for i in range(len(str1)-1):
-        temp = p.match(str1[i:i+2])
-        if temp:
-            check1.append(temp.group())
+        s = str1[i:i+2].lower()
+        if s.isalpha():
+            total += 1
+            if s in check:
+                check[s] += 1
+            else:
+                check[s] = 1
+    nset = 0
     for i in range(len(str2)-1):
-        temp = p.match(str2[i:i+2])
-        if temp:
-            check2.append(temp.group())
-    print(check1)
-    print(check2)
-    if len(check1) == 0:
-        if len(check2) == 0: answer = 65536
-        else: answer = 0    
-    else:
-        cnt = 0
-        total = len(check1)+len(check2)
-        for i in check1:
-            if i in check2:
-                cnt += 1
-                check2.remove(i) 
-        print(cnt)
-        answer = math.floor((cnt/(total-cnt))*65536)
-    return answer
+        s = str2[i:i+2].lower()
+        if s.isalpha():
+            if s in check:
+                nset += 1
+                check[s] -= 1
+                if check[s] == 0:
+                    del check[s]
+            else:
+                total += 1
+    res = 1 if total==nset else nset/total
+    return int(res*65536)
