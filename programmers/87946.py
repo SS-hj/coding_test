a50b3a421c1e1fce1220d9b89c1e3f267fb8a68e
SS-hj <gkwjd111@naver.com>
2023-10-16 18:06:21 +0900

    Update code

--- programmers/87946.py
+++ programmers/87946.py
@@ -1,14 +1,13 @@
 from itertools import permutations
-
 def solution(k, dungeons):
-    N = len(dungeons)
-    plays = list(permutations(dungeons,N))
-    ans = 0
-    for play in plays:
-        cnt = 0; power = k
-        for j in range(N):
-            if power >= play[j][0]:
-                power-=play[j][1]
-                cnt+=1
-        ans = max(ans,cnt)
-    return ans
+    totalRes = 0
+    for p in permutations(dungeons):
+        power = k
+        res = 0
+        for a, b in p:
+            if a > power:
+                break
+            power -= b
+            res += 1
+        totalRes = max(totalRes, res)
+    return totalRes
