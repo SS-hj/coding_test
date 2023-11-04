61e1d689a3f62759dbcc41a1407207c77875b2cf
SS-hj <gkwjd111@naver.com>
2023-11-04 12:31:41 +0900

    Update code

--- programmers/17679.py
+++ programmers/17679.py
@@ -1,27 +1,36 @@
-from collections import deque
-
 def solution(m, n, board):
     cnt = 0
-    arr = [list(b) for b in board]
-    while True:
-        temp = set()
+    turn = True
+    board = [list(board[r]) for r in range(m)]
+    def check(turn):
+        turn = False
+        visited = set()
         for r in range(m-1):
             for c in range(n-1):
-                if arr[r][c] and arr[r][c]==arr[r+1][c]==arr[r][c+1]==arr[r+1][c+1]:
-                    temp.update([(r,c),(r+1,c),(r,c+1),(r+1,c+1)])
-        if not temp:
-            break
-        for r, c in temp:
-            arr[r][c] = ""
+                if board[r][c] and board[r][c]==board[r][c+1]==board[r+1][c]==board[r+1][c+1]:
+                    visited.add((r,c))
+                    visited.add((r,c+1))
+                    visited.add((r+1,c))
+                    visited.add((r+1,c+1))
+        if visited:
+            delete(visited)
+            turn = True
+        return turn
+    def delete(visited):
+        nonlocal cnt
+        for r, c in visited:
             cnt += 1
+            board[r][c] = ''
         for c in range(n):
-            temp = deque()
+            temp = []
+            for r in range(m):
+                if board[r][c]:
+                    temp.append((board[r][c]))
+                    board[r][c] = ''
             for r in range(m-1,-1,-1):
-                if arr[r][c]:
-                    temp.append(arr[r][c])
-            for r in range(m-1,-1,-1):
-                try:
-                    arr[r][c] = temp.popleft()
-                except:
-                    arr[r][c] = ""
+                if not temp:
+                    break
+                board[r][c] = temp.pop()
+    while turn:
+        turn = check(turn)
     return cnt
